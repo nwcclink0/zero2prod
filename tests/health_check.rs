070f605dd1fced2037decf9bec40f79e414811f7
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-11 09:38:41 +0800

    feat: Let tracing could have alternative choice to log output.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,16 +1,23 @@
 use std::net::TcpListener;
 
+use once_cell::sync::Lazy;
 use sqlx::Executor;
 use sqlx::{Connection, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::configuration::get_configuration;
 use zero2prod::configuration::DatabaseSettings;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use once_cell::sync::Lazy;
 
-static TRACING: Lazy<()> = Lazy::new(||{
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber(subscriber);
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 pub struct TestApp {
