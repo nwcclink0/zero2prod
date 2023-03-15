6a859ea33f3ba7a354fdc12d90aab86a84ce0da9
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-16 04:43:28 +0800

    feat: Add http client timeout handler.

--- src/email_client.rs
+++ src/email_client.rs
@@ -16,8 +16,13 @@ impl EmailClient {
         sender: SubscriberEmail,
         authorization_token: Secret<String>,
     ) -> Self {
+        let http_client = Client::builder()
+            .timeout(std::time::Duration::from_secs(10))
+            .build()
+            .unwrap();
+
         Self {
-            http_client: Client::new(),
+            http_client,
             base_url,
             sender,
             authorization_token,
@@ -64,7 +69,6 @@ struct SendEmailRequest<'a> {
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Sub;
 
     use crate::domain::SubscriberEmail;
     use crate::email_client::EmailClient;
@@ -174,15 +178,16 @@ mod tests {
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
 
-        let response = ResponseTemplate::new(200)
-            .set_delay(std::time::Duration::from_secs(180));
+        let response = ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180));
         Mock::given(any())
             .respond_with(response)
             .expect(1)
             .mount(&mock_server)
             .await;
 
-        let outcome = email_client.send_email(subscriber_email, &subject, &content, &content).await;
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
 
         assert_err!(outcome);
     }
