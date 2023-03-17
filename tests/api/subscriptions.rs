4abdfcabff2ea641312f650fa040f2ba523a0e03
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-18 05:17:58 +0800

    feat: Add MockServer for email server.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,3 +1,8 @@
+use wiremock::{
+    matchers::{method, path},
+    Mock, ResponseTemplate,
+};
+
 use crate::helpers::spawn_app;
 
 #[tokio::test]
@@ -5,8 +10,14 @@ async fn subscribe_returns_a_200_for_vaild_form_data() {
     let app = spawn_app().await;
 
     let body = "name=jabber&email=jabber-shelves0n@icloud.com";
-    let response = app.post_subscriptions(body.into()).await;
 
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_subscriptions(body.into()).await;
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("Select email, name from subscriptions",)
@@ -57,3 +68,36 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=jabber&email=jabber-shelves0n@icloud.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+}
+
+// #[tokio::test]
+// async fn subscribe_sends_a_confirmation_email_with_a_link() {
+//     let app = spawn_app().await;
+//     let body = "name=jabber&email=jabber-shelves0n@icloud.com";
+//
+//     Mock::given(path("/email"))
+//         .and(method("POST"))
+//         .respond_with(ResponseTemplate::new(200))
+//         .mount(&app.email_server)
+//         .await;
+//
+//     app.post_subscriptions(body.into()).await;
+//
+//     let email_request = &app.email_server.received_requests().await.unwrap()[0];
+//
+//     let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+// }
