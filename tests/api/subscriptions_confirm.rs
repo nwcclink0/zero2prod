3b201bbfa6a4f1691389eb68d90b83e0192dcb93
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-19 06:23:49 +0800

    feat: Add confirmation links.

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,5 +1,5 @@
 use crate::helpers::spawn_app;
-use reqwest::Url;
+
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -26,23 +26,42 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
         .await;
 
     app.post_subscriptions(body.into()).await;
-    let email_reqeuest = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_reqeuest.body).unwrap();
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
 
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
+    let confirmation_links = app.get_confirmation_links(&email_request);
 
-    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
-    let confirmation_link = Url::parse(raw_confirmation_link).unwrap();
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-
-    let response = reqwest::get(confirmation_link).await.unwrap();
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn clicking_on_the_link_confirms_a_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=jabber&email=jabber-shelves0n@icloud.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
+    reqwest::get(confirmation_links.html)
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch one saved subscriptions");
+
+    assert_eq!(saved.email, "jabber-shelves0n@icloud.com");
+    assert_eq!(saved.name, "jabber");
+    assert_eq!(saved.status, "confirmed");
 }
