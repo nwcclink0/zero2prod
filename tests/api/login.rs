a22b9911b71106f121250525bbdd4fb4b71e260c
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-25 13:06:26 +0800

    feat: Use cookie instead of query parameter.

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -6,7 +6,7 @@ use crate::helpers::{assert_is_redirect_to, spawn_app};
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
     let app = spawn_app().await;
-    //Act
+    //Act 1, try login
     let login_body = serde_json::json!({
         "username":"random-username",
         "password":"random-password"
@@ -26,7 +26,11 @@ async fn an_error_flash_message_is_set_on_failure() {
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
 
-    //Act part 2
+    //Act part 2, Follow the redirect
     let html_page = app.get_login_html().await;
-    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#))
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    //Act part 3, Reload the login page
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#))
 }
