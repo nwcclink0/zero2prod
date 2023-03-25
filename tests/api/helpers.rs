35927cb1f5934bb535ac79cad95635a6a6b4bc3b
yuantingwei <2349731+nwcclink0@users.noreply.github.com>
2023-03-25 10:24:54 +0800

    feat: Add cookie store from actix_web.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -28,6 +28,7 @@ pub struct TestApp {
     pub db_pool: PgPool,
     pub email_server: MockServer,
     pub test_user: TestUser,
+    pub api_client: reqwest::Client,
 }
 
 pub struct ConfirmationLinks {
@@ -40,10 +41,7 @@ impl TestApp {
     where
         Body: serde::Serialize,
     {
-        reqwest::Client::builder()
-            .redirect(reqwest::redirect::Policy::none())
-            .build()
-            .unwrap()
+        self.api_client
             .post(&format!("{}/login", &self.address))
             .form(body)
             .send()
@@ -52,7 +50,7 @@ impl TestApp {
     }
 
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/subscriptions", &self.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
@@ -83,13 +81,24 @@ impl TestApp {
     }
 
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
-        reqwest::Client::new()
+        self.api_client
             .post(&format!("{}/newsletters", &self.address))
             .basic_auth(&self.test_user.username, Some(&self.test_user.password))
             .json(&body)
             .send()
             .await
             .expect("Failed to execute request")
+    }
+
+    pub async fn get_login_html(&self) -> String {
+        self.api_client
+            .get(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
     }
 }
 
@@ -113,12 +122,18 @@ pub async fn spawn_app() -> TestApp {
         .expect("Failed to build application.");
     let application_port = application.port();
     let _ = tokio::spawn(application.run_until_stopped());
+    let client = reqwest::Client::builder()
+        .redirect(reqwest::redirect::Policy::none())
+        .cookie_store(true)
+        .build()
+        .unwrap();
     let test_app = TestApp {
         address: format!("http://localhost:{}", application_port),
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
         email_server,
         test_user: TestUser::generate(),
+        api_client: client,
     };
     test_app.test_user.store(&test_app.db_pool).await;
     test_app
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,12 +1,12 @@
-use std::collections::HashSet;
 use reqwest::header::HeaderValue;
+use std::collections::HashSet;
 
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
     let app = spawn_app().await;
-
+    //Act
     let login_body = serde_json::json!({
         "username":"random-username",
         "password":"random-password"
@@ -21,10 +21,12 @@ async fn an_error_flash_message_is_set_on_failure() {
         .get_all("Set-Cookie")
         .into_iter()
         .collect();
-    assert!(
-        cookies
-        .contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
+    assert!(cookies.contains(&HeaderValue::from_str("_flash=Authentication failed").unwrap()));
 
     let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
     assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    //Act part 2
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#))
 }
